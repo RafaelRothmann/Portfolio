ca98bb1dbee9e3c877dc936e9ba1f32e596332bf
RafaelRothmann <rafinharothmann@gmail.com>
2023-10-05 03:14:45 -0300

    Added footer to index.html

--- source/js/sistem.js
+++ source/js/sistem.js
@@ -1,13 +1,3 @@
-function mudarImagemnoticias(){
-    const news = ["archive/images/certificates/certificate-Lua.jpg","archive/images/certificates/certificate-Lua.jpg","archive/images/certificates/certificate-Lua.jpg"];
-
-    const id_imgNews = ["n1-img","n2-img","n3-img"]
-
-    for (let index = 0; index < news.length; index++) {
-        document.getElementById(id_imgNews[index]).src=news[index];
-    }
-}
-
 function mostrarLinguagens() {
     document.getElementById("options").style = "visibility: visible;";
 }
