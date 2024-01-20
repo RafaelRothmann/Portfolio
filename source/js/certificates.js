b26dd2e22b0c4e5ee5a40f6cd38d32e064d4748b
RafaelRothmann <rafinharothmann@gmail.com>
2024-01-20 08:09:22 -0300

    Added certificates automation

--- source/js/certificates.js
+++ source/js/certificates.js
@@ -15,8 +15,6 @@ class certificates {
                 break;
         }
 
-
-
         window.location.href = "certificates.html?" + lang + "&" + course;
     }
 
@@ -35,146 +33,157 @@ class certificates {
         this.changePage(params['course'], params['lang']);
     }
 
-    static changePage(course, lang) {
-        translation.mudarIdioma(lang, 'certificates');
+    static changePage(course, language) {
+        translation.mudarIdioma(language, 'certificates');
         sistem.versaoSite();
-        var arquivo, titulo, desc1, desc2, desc3, desc4;
+        var img, titulo, desc1, desc2, desc3, desc4, lang;
+        var found = false;
 
-        switch (course) {
-            case 'LuaUDEMY':
-                switch (lang) {
-                    case "pt-br":
-                        titulo = "Aprenda a programar em Lua";
-                        desc1 = "Empresa: Udemy";
-                        desc2 = "Duração: 4 horas";
-                        desc3 = "Emitida em Setembro de 2023";
-                        desc4 = "Código da credencial: UC-fa5e6ff6-148a-4c92-8d9e-8a09e53008d8";
-
-                        break;
-                    case "us":
-                        titulo = "Learn to program in Lua";
-                        desc1 = "Company: Udemy";
-                        desc2 = "Duration: 4 hours";
-                        desc3 = "Issued on September 2023";
-                        desc4 = "Credential Code: UC-fa5e6ff6-148a-4c92-8d9e-8a09e53008d8";
-
-                        break;
-                }
-
-                arquivo = "LuaUDEMY.jpg";
-
-                break;
-            case 'MCGITHUB2023':
-                switch (lang) {
-                    case "pt-br":
-                        titulo = "Conceitos Básicos de GitHub - Noções básicas de administração e recursos do produto";
-                        desc1 = "Empresa: Microsoft";
-                        desc2 = "Duração: 1 hora 52 minutos";
-                        desc3 = "Emitida em Maio de 2023";
-                        desc4 = " ";
-
-                        break;
-                    case "us":
-                        titulo = "Basic Concepts of GitHub - Basic Notions of Administration and Product Features";
-                        desc1 = "Company: Microsoft";
-                        desc2 = "Duration: 1 hour 52 minutes";
-                        desc3 = "Issued on May 2023";
-                        desc4 = " ";
-
-                        break;
-                }
-
-                arquivo = "MCGITHUB2023.jpg";
-                break;
-            case 'ERAD2023':
-                switch (lang) {
-                    case "pt-br":
-                        titulo = "XXIII Escola Regional de Alto Desempenho da Região Sul";
-                        desc1 = "Empresa: Sociedade Brasileira de Computação (SBC)";
-                        desc2 = "Duração: 24 horas";
-                        desc3 = "Emitida em Maio de 2023";
-                        desc4 = " ";
-
-                        break;
-                    case "us":
-                        titulo = "XXIII Regional High Performance School of the Southern Region";
-                        desc1 = "Company: Sociedade Brasileira de Computação (SBC)";
-                        desc2 = "Duration: 24 hours";
-                        desc3 = "Issued on May 2023";
-                        desc4 = " ";
-
-                        break;
-                }
-
-                arquivo = "ERAD2023.jpg";
-
-                break;
-            case 'JavaSpringBoot-2023':
-                switch (lang) {
-                    case 'pt-br':
-                        titulo = "Curso Online de Java";
-                        desc1 = "Empresa: Rockseat";
-                        desc2 = "Duração: 4 horas";
-                        desc3 = "Emitida em Outubro de 2023";
-                        desc4 = "Código da credencial: 69aa42ef-6251-41ca-9e71-e33803a8a40c";
-
-
-                        break;
-                    case 'us':
-                        titulo = "Online Java Course";
-                        desc1 = "Company: Rockseat";
-                        desc2 = "Duration: 4 hours";
-                        desc3 = "Issued on October 2023";
-                        desc4 = "Credential Code: 69aa42ef-6251-41ca-9e71-e33803a8a40c";
-
-                        break;
-                }
-
-                arquivo = 'JavaSpringBoot-2023.png';
-                break;
-            case 'Cambly':
-                switch (lang) {
-                    case 'pt-br':
-                        titulo = "Certificado de Conclusão Cambly";
-                        desc1 = "Empresa: Cambly";
-                        desc2 = "Duração: 12 horas";
-                        desc3 = "Emitida em Novembro de 2023";
-                        desc4 = "Código da credencial: f3fe2054";
-
-                        break;
-                    case 'us':
-                        titulo = "Cambly Certificate of Accomplishment";
-                        desc1 = "Company: Cambly";
-                        desc2 = "Duration: 12 hours";
-                        desc3 = "Issued on November 2023";
-                        desc4 = "Código da credencial: f3fe2054";
-
-                        break;
-                }
-                
-                arquivo = 'Cambly.png'
-            break;
-            default:
-
-                titulo = "Error File 404";
-                arquivo = "";
-                desc1 = "O Curso de codigo <b>" + course + "</b> não foi possível ser encontrado!";
-                desc2 = "Na linguagem <b>" + lang + "</b>!!";
-                desc3 = "Porfavor entre em contato nós enviando essa imagem ao email: ";
-                desc4 = "<a href='mailto:r.m.rothmann@gmail.com?subject=Imagem não encontrada!!&body=" + desc1.replace("<b>", "").replace("</b>", "") +
-                    " " + desc2.replace("<b>", "").replace("</b>", "") + "'>r.m.rothmann@gmail.com</a>";
-
-                break;
-
+        if (language == 'pt-br') {
+            lang = 0;
+        } else if (language == 'us') {
+            lang = 1;
         }
 
-        document.getElementById('titleCertificate').innerHTML = titulo;
-        document.getElementById('desc1').innerHTML = desc1;
-        document.getElementById('desc2').innerHTML = desc2;
-        document.getElementById('desc3').innerHTML = desc3;
-        document.getElementById('desc4').innerHTML = desc4;
-        document.getElementById("course").src = "../../archive/images/certificates/certificate-" + arquivo;
-        document.getElementById("download").href = "../../archive/images/certificates/certificate-" + arquivo;
+        fetch('../db/certificates.json').then((response) => {
+            response.json().then((certificates) => {
+                for (let index = 0; index < certificates.length; index++) {
+                    if (certificates[index]['codigo'] == course) {
+                        found = true;
+
+                        titulo = certificates[index]['titulo'][lang];
+                        desc1 = certificates[index]['org'];
+                        desc2 = certificates[index]['emissao'];
+                        desc3 = certificates[index]['duracao'];
+                        desc4 = certificates[index]['credencial'];
+                        img = "../../archive/images/certificates/certificate-" + certificates[index]['codigo'] + certificates[index]['tipo'];
+
+                    }
+
+                }
+
+                if (!found) {
+                    this.procurarEventos(found, lang, language, course);
+                } else {
+                    this.preencherCampos(titulo, desc1, desc2, desc3, desc4, img, language, found);
+                }
+
+            });
+        });
+
+
+
+
     }
+
+
+    static procurarEventos(found, lang, language, course) {
+        var img, titulo, desc1, desc2, desc3, desc4, lang;
+
+        fetch('../db/events.json').then((response) => {
+            response.json().then((events) => {
+                for (let index = 0; index < events.length; index++) {
+                    if (events[index]['codigo'] == course) {
+                        found = true;
+
+                        titulo = events[index]['titulo'][lang];
+                        desc1 = events[index]['org'];
+                        desc2 = events[index]['emissao'];
+                        desc3 = events[index]['duracao'];
+                        desc4 = events[index]['credencial'];
+                        img = "../../archive/images/certificates/certificate-" + events[index]['codigo'] + events[index]['tipo'];
+
+
+                    }
+
+                }
+
+                if (!found) {
+                    titulo = "Error File 404";
+                    img = "../../archive/images/other/construcao.png";
+                    desc1 = "O Curso de codigo <b>" + course + "</b> não foi possível ser encontrado!";
+                    desc2 = "Na linguagem <b>" + language + "</b>!!";
+                    desc3 = "Porfavor entre em contato nós enviando essa imagem ao email: ";
+                    desc4 = "<a href='mailto:r.m.rothmann@gmail.com?subject=Imagem não encontrada!!&body=" + desc1.replace("<b>", "").replace("</b>", "") +
+                        " " + desc2.replace("<b>", "").replace("</b>", "") + "'>r.m.rothmann@gmail.com</a>";
+                }
+
+                this.preencherCampos(titulo, desc1, desc2, desc3, desc4, img, language, found);
+
+            });
+        });
+    }
+
+    static preencherCampos(titulo, desc1, desc2, desc3, desc4, img, language, found) {
+
+        var titleCertificate = document.createElement("h2")
+        titleCertificate.innerText = titulo;
+        titleCertificate.id = "titleCertificate";
+
+        var a = document.createElement("a");
+        a.id = "download";
+        a.target = "_blank";
+        a.href = img;
+
+        var course = document.createElement("img");
+        course.id = "course";
+        course.src = img;
+
+        var div_desc = document.createElement("div");
+        div_desc.id = "desc";
+
+        var desc_1 = document.createElement("p");
+        desc_1.id = "desc1";
+
+        if (language == 'pt-br' && found){
+            desc_1.innerText = "Empresa: " + desc1;
+        } else if (language == 'us' && found) {
+            desc_1.innerText = "Company: " + desc1;
+        } else {
+            desc_1.innerHTML = desc1;
+        }
+
+        div_desc.appendChild(desc_1);
+        
+        var desc_2 = document.createElement("p");
+        desc_2.id = "desc2";
+        desc_2.className = "time";
+        desc_2.innerHTML = desc2;
+        
+
+        div_desc.appendChild(desc_2);
+    
+        var desc_3 = document.createElement("p");
+        desc_3.id = "desc3";
+        desc_3.className = "time";
+        desc_3.innerHTML = desc3;
+        
+
+        div_desc.appendChild(desc_3);
+
+        var desc_4;
+
+        if (desc4 != null) {
+            desc_4 = document.createElement("p");
+            desc_4.id = "desc4";
+            desc_4.className = "credencial";
+            desc_4.innerHTML = desc4;
+            
+
+            div_desc.appendChild(desc_4);
+        }
+
+        a.appendChild(course);
+        
+
+        var section = document.getElementById("certificate");
+        section.appendChild(titleCertificate);
+        section.appendChild(a);
+        section.appendChild(div_desc);
+
+        translation.traduzirTime(language);
+        translation.traduzirCredencial(language);
+    }
+
 
 }
