e49f953b76c08637c17aa2b8cb20efd0b62a3590
RafaelRothmann <rafinharothmann@gmail.com>
2024-02-13 21:30:06 -0300

    Added automation footer on all pages

--- source/js/page.js
+++ source/js/page.js
@@ -1,6 +1,6 @@
 class page {
     static gerrarHeader(){
-        let header = document.createElement("header");
+        let header = document.getElementById("header");
     
         let h1 = document.createElement("h1");
         let a_h1 = document.createElement("a");
@@ -91,9 +91,59 @@ class page {
         header.appendChild(h1);
         header.appendChild(nav);
     
-        document.getElementById("body").appendChild(header);
+        this.gerrarFooter();
     }
     
+    static gerrarFooter() {
+        let footer = document.getElementById("footer");
+
+        let div_ft = document.createElement("div")
+        div_ft.id = "ft";
+
+        let div_resum_footer = document.createElement("div")
+        div_resum_footer.id = "resum-footer";
+
+        let h1 = document.createElement("h1")
+        h1.innerText = "RAFAEL ROTHMANN";
+
+        let p_resum2 = document.createElement("p")
+        p_resum2.id = "resum2"
+        p_resum2.innerText = "Tenho 19 anos e este é o meu site de portfolio, onde compartilho os projetos e eventos em que participei ao longo dos anos. Aqui, você encontrará uma variedade de trabalhos que representam meu crescimento e dedicação na area da Tecnologia. Espero que essas experiências demonstrem minha paixão pelo aprendizado e minha determinação em contribuir de forma significativa em diferentes projetos e eventos.";
+
+        div_resum_footer.appendChild(h1)
+        div_resum_footer.appendChild(p_resum2)
+
+        let div_lin = document.createElement("div")
+        div_lin.id = "lin"
+
+        let ul_link_1 = document.createElement("ul")
+        ul_link_1.className = "link"
+        ul_link_1.innerHTML= "<li><h6 id='allC'>Todos os Conteúdos</h6></li><li><a id='aM'>Sobre mim</a></li><li><a  id='lN'>Ultimas Noticias</a></li><li><a id='cV'>Currículo</a></li><li><a id='pJ'>Projetos</a></li><li><a id='cTT'>Contato</a></li>"
+
+        let ul_link_2 = document.createElement("ul")
+        ul_link_2.className = "link"
+        ul_link_2.innerHTML = "<li><h6 id='fA'>Formação Academica</h6></li><li><a id='eV'>Eventos</a></li><li><a id='gR'>Gradução</a></li><li><a id='cR'>Cursos</a></li>";
+
+        let ul_link_3 = document.createElement("ul")
+        ul_link_3.className = "link"
+        ul_link_3.innerHTML = "<li><h6 id='projects'>Projetos</h6></li><li><a href='https://github.com/RafaelRothmann/Portfolio' id='wS' target='_blank'>Protifolio</a></li><li><a href='https://github.com/RafaelRothmann/Chess-System_Java-Project' id='cS' target='_blank'>Chess System</a></li><li><a href='https://github.com/RafaelRothmann/Beecrowd-Cpp' id='bC' target='_blank'>Beecrowd Cpp</a></li>"
+
+        div_lin.appendChild(ul_link_1)
+        div_lin.appendChild(ul_link_2)
+        div_lin.appendChild(ul_link_3)
+
+        div_ft.appendChild(div_resum_footer)
+        div_ft.appendChild(div_lin)
+
+        let div_c = document.createElement("div")
+        div_c.innerHTML = "<p id='credits'></p>"
+
+        footer.appendChild(div_ft)
+        footer.appendChild(div_c)
+
+        console.log(footer)
+
+    }
 }
 
 
